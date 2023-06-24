cba7dab4291b68ca3e26e829b3b3ba650a7aa582
Ethan Hunt <chessking94@gmail.com>
2023-06-23 22:25:45 -0500

    Implement log write for fileproc

--- src/fileproc.py
+++ src/fileproc.py
@@ -13,6 +13,7 @@ import os
 import tempfile
 import shutil
 
+from .constants import BOOLEANS as BOOLEANS
 from .constants import NL as NL
 from .misc import get_config as get_config
 
@@ -21,7 +22,8 @@ from .misc import get_config as get_config
 
 class monitoring_constants:
     MODULE_NAME = os.path.splitext(os.path.basename(__file__))[0]
-    DELIM = get_config(MODULE_NAME, 'referenceDelimiter')
+    DELIM = get_config(MODULE_NAME, 'logDelimiter')
+    REFDELIM = get_config(MODULE_NAME, 'referenceDelimiter')
 
 
 class monitoring:
@@ -31,6 +33,9 @@ class monitoring:
         self.last_review_time = self._processtime(readwrite='r')
         self.manual_review = False
 
+        self.log_path = get_config(monitoring_constants.MODULE_NAME, 'logPath')
+        self.log_name = f"{get_config(monitoring_constants.MODULE_NAME, 'logName')}_{dt.datetime.now().strftime('%Y%m%d%H%M%S')}.log"
+
         if self._validate() is not None:
             logging.critical(self.error)
 
@@ -38,7 +43,7 @@ class monitoring:
         if not os.path.isdir(self.path):
             self.error = f'Path does not exist|{self.path}'
 
-        if monitoring_constants.DELIM in self.path:
+        if monitoring_constants.REFDELIM in self.path:
             self.error = f'Path contains referenceDelimiter|{self.path}'
 
         return self.error
@@ -63,7 +68,7 @@ class monitoring:
         dt_column = 'LastMonitorTime'
         reference_file = get_config(monitoring_constants.MODULE_NAME, 'referenceFile')
         if not os.path.isfile(reference_file):
-            header_row = f'{key_column}{monitoring_constants.DELIM}{dt_column}{NL}'
+            header_row = f'{key_column}{monitoring_constants.REFDELIM}{dt_column}{NL}'
             with open(file=reference_file, mode='w', encoding='utf-8') as f:
                 f.write(header_row)
 
@@ -79,7 +84,6 @@ class monitoring:
             if time_val is None:
                 time_val = dt.datetime(1970, 1, 1)
         else:
-            logging.critical('going to update file')
             temp_file_path = tempfile.NamedTemporaryFile(delete=False).name
             with open(reference_file, 'r', newline='') as reader_file, open(temp_file_path, 'w', newline='') as writer_file:
                 reader = csv.DictReader(reader_file)
@@ -104,11 +108,18 @@ class monitoring:
 
         return time_val
 
+    def _writelog(self, filename: str):
+        with open(os.path.join(self.log_path, self.log_name), 'a') as logfile:
+            dte, tme = dt.datetime.now().strftime('%Y-%m-%d'), dt.datetime.now().strftime('%H:%M:%S')
+            logfile.write(f'{self.path}{monitoring_constants.DELIM}{dte}{monitoring_constants.DELIM}')
+            logfile.write(f'{tme}{monitoring_constants.DELIM}{filename}{NL}')
+
     def change_time(self, dt_string: str):
         self.last_review_time = self._processtime(readwrite='r', dt_string=dt_string)
         self.manual_review = True
 
-    def modified_files(self) -> list:
+    def modified_files(self, write_log: bool = False) -> list:
+        write_log = write_log if write_log in BOOLEANS else False
         directory_files = os.listdir(self.path)
         mod_files = []
         for file in directory_files:
@@ -120,5 +131,9 @@ class monitoring:
 
         if not self.manual_review:
             self._processtime(readwrite='w')
+
+        if write_log:
+            for f in mod_files:
+                self._writelog(f)
 
         return mod_files
