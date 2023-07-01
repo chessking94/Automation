340fc210a7f25616845424e243e51caf2ea861b5
Ethan Hunt <chessking94@gmail.com>
2023-07-01 06:47:20 -0500

    Return filename for all functions

--- src/office.py
+++ src/office.py
@@ -16,18 +16,20 @@ import xlsxwriter as xl
 
 from .constants import BOOLEANS as BOOLEANS
 
+# TODO: Add delimiter change support to convert()
+
 
 class convert():
     def __init__(self):
         pass
 
-    def extract_columns(self, filename: str, columns: list):
+    def extract_columns(self, filename: str, columns: list) -> str:
         if not os.path.isfile(filename):
             raise FileNotFoundError
 
+        output_file = f'{os.path.splitext(filename)[0]}_filtered{os.path.splitext(filename)[1]}'
         ext = os.path.splitext(filename)[1].lower()
         if ext in ['.csv', '.txt']:
-            output_file = f'{os.path.splitext(filename)[0]}_filtered{os.path.splitext(filename)[1]}'
             if os.path.isfile(output_file):
                 raise FileExistsError
 
@@ -35,11 +37,15 @@ class convert():
             df_filtered = df[columns]
             df_filtered.to_csv(output_file, index=False)
         elif ext in ['.xlsx', '.xls']:
+            # TODO: Extract Excel columns
             logging.warning(f'Excel conversion not yet developed|{filename}')
         else:
+            output_file = None
             logging.critical(f'Other extensions not currently supported|{filename}')
 
-    def csv_to_excel(self, filename: str, delim: str = None):
+        return output_file
+
+    def csv_to_excel(self, filename: str, delim: str = None) -> str:
         if not os.path.isfile(filename):
             raise FileNotFoundError
 
@@ -50,12 +56,13 @@ class convert():
         path = os.path.dirname(filename)
         file = os.path.basename(filename)
         new_file = f'{os.path.splitext(file)[0]}.xlsx'  # always going to xlsx
+        output_file = os.path.join(path, new_file)
 
-        if os.path.isfile(os.path.join(path, new_file)):
+        if os.path.isfile(output_file):
             logging.warning(f'File {file} has already been converted to Excel')
         else:
             df = pd.read_csv(filename)
-            wb = xl.Workbook(os.path.join(path, new_file))
+            wb = xl.Workbook(output_file)
             ws = wb.add_worksheet()
             text_format = wb.add_format({'num_format': '@'})
             for i, column in enumerate(df.columns):
@@ -68,7 +75,9 @@ class convert():
             if os.path.isdir(os.path.join(path, 'Archive')):
                 os.rename(filename, os.path.join(path, 'Archive', file))
 
-    def word_to_pdf(self, filename):
+        return output_file
+
+    def word_to_pdf(self, filename: str) -> str:
         if not os.path.isfile(filename):
             raise FileNotFoundError
 
@@ -79,15 +88,18 @@ class convert():
         path = os.path.dirname(filename)
         file = os.path.basename(filename)
         new_file = f'{os.path.splitext(file)[0]}.pdf'
+        output_file = os.path.join(path, new_file)
 
         word = client.Dispatch('Word.Application')  # requires Office to be installed
-        if os.path.exists(new_file):
-            os.remove(new_file)
+        if os.path.isfile(output_file):
+            os.remove(output_file)
         worddoc = word.Documents.Open(filename, ReadOnly=1)
-        worddoc.SaveAs(os.path.join(path, new_file), FileFormat=17)
+        worddoc.SaveAs(output_file, FileFormat=17)
         worddoc.Close()
 
-    def excel_to_csv(self, filename: str, delim: str = ','):
+        return output_file
+
+    def excel_to_csv(self, filename: str, delim: str = ',') -> str:
         if not os.path.isfile(filename):
             raise FileNotFoundError
 
@@ -106,22 +118,25 @@ class convert():
         path = os.path.dirname(filename)
         file = os.path.basename(filename)
         new_file = os.path.splitext(file)[0] + ext
+        output_file = os.path.join(path, new_file)
 
         if os.path.isfile(os.path.join(path, new_file)):
             logging.warning(f'File {file} has already been converted to csv')
         else:
             df = pd.read_excel(filename, engine='openpyxl')
-            df.to_csv(os.path.join(path, new_file), sep=delim, encoding='utf-8', index=False)
+            df.to_csv(output_file, sep=delim, encoding='utf-8', index=False)
 
             if os.path.isdir(os.path.join(path, 'Archive')):
                 os.rename(filename, os.path.join(path, 'Archive', file))
+
+        return output_file
 
 
 class excel():
     def __init__(self):
         pass
 
-    def refresh_file(self, filename: str, save_copy: bool = True):
+    def refresh_file(self, filename: str, save_copy: bool = True) -> str:
         save_copy = save_copy if save_copy in BOOLEANS else True
 
         excel = client.gencache.EnsureDispatch('Excel.Application')
@@ -134,17 +149,21 @@ class excel():
                 if pt.PivotCache().RecordCount == 0:
                     pt.RefreshTable()
 
+        output_file = filename
         if save_copy:
             dte = dt.datetime.now().strftime('%Y%m%d %H%M')
             new_filename = f'{os.path.splitext(filename)[0]} {dte}{os.path.splitext(filename)[1]}'
             wb.SaveAs(new_filename)
+            output_file = new_filename
         else:
             wb.Save()
 
         wb.Close()
         excel.Quit()
 
-    def run_vba(self, filename: str, macro_name: str = None, save_copy: bool = True):
+        return output_file
+
+    def run_vba(self, filename: str, macro_name: str = None, save_copy: bool = True) -> str:
         save_copy = save_copy if save_copy in BOOLEANS else True
 
         excel = client.Dispatch('Excel.Application')
@@ -164,12 +183,16 @@ class excel():
         else:
             excel.Application.Run(macro_name)
 
+        output_file = filename
         if save_copy:
             dte = dt.datetime.now().strftime('%Y%m%d %H%M')
             new_filename = f'{os.path.splitext(filename)[0]} {dte}{os.path.splitext(filename)[1]}'
             wb.SaveAs(new_filename)
+            output_file = new_filename
         else:
             wb.Save()
 
         wb.Close()
         excel.Quit()
+
+        return output_file
