614aa1750854a47f0ce13417b7dcbf53b363fc15
Ethan Hunt <chessking94@gmail.com>
2023-07-01 13:21:50 -0500

    Convert test_cmd to using setUp

--- test/test_cmd.py
+++ test/test_cmd.py
@@ -8,27 +8,26 @@ FILE_DIR = os.path.join(os.path.dirname(__file__), 'files')
 
 
 class TestCmd(unittest.TestCase):
+    def setUp(self):
+        self.script = cmd()
+
     def test_run_script_with_parameters(self):
-        script = cmd()
-        test_val = script.run_script('py', FILE_DIR, 'cmd_script.py', '-a1 -b2')
+        test_val = self.script.run_script('py', FILE_DIR, 'cmd_script.py', '-a1 -b2')
 
         self.assertEqual(test_val, 0)
 
     def test_run_script_without_parameters(self):
-        script = cmd()
-        test_val = script.run_script('py', FILE_DIR, 'cmd_script.py')
+        test_val = self.script.run_script('py', FILE_DIR, 'cmd_script.py')
 
         self.assertNotEqual(test_val, 0)
 
     def test_run_script_without_program_name(self):
-        script = cmd()
-        test_val = script.run_script(None, FILE_DIR, 'cmd_script.bat')
+        test_val = self.script.run_script(None, FILE_DIR, 'cmd_script.bat')
 
         self.assertEqual(test_val, 0)
 
     def test_run_script_with_invalid_script_path(self):
-        script = cmd()
-        self.assertRaises(FileNotFoundError, script.run_script, 'py', FILE_DIR, 'dne.py')
+        self.assertRaises(FileNotFoundError, self.script.run_script, 'py', FILE_DIR, 'dne.py')
 
 
 if __name__ == '__main__':
