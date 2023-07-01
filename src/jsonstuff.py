f3226aeb83055bb77f52cdbeae912eababc28d97
Ethan Hunt <chessking94@gmail.com>
2023-07-01 13:52:01 -0500

    Return list of files reformatted

--- src/jsonstuff.py
+++ src/jsonstuff.py
@@ -7,28 +7,29 @@ Version: 1.0
 """
 
 import fnmatch
-import logging
 import os
 import json
 
 
-def reformat_json(path: str, file: str = None):
+def reformat_json(path: str, file: str = None) -> list:
     if not os.path.isdir(path):
-        logging.critical(f'Path {path} does not exist!')
+        raise FileNotFoundError(f'Path {path} does not exist!')
 
+    name_append = '_reformat'
     json_list = []
     if file is None:
         for f in os.listdir(path):
-            if fnmatch.fnmatch(f, '*.json'):
+            if fnmatch.fnmatch(f, '*.json') and name_append not in f:
                 json_list.append(f)
     else:
         if os.path.isfile(os.path.join(path, file)):
             json_list.append(file)
         else:
-            logging.critical(f'File {file} does not exist!')
+            raise FileNotFoundError(f'File {file} does not exist!')
 
+    file_list = []
     for json_orig in json_list:
-        json_reformat = f'{os.path.splitext(json_orig)[0]}_reformat.json'
+        json_reformat = f'{os.path.splitext(json_orig)[0]}{name_append}.json'
         orig_file = os.path.join(path, json_orig)
         reformat_file = os.path.join(path, json_reformat)
 
@@ -37,3 +38,6 @@ def reformat_json(path: str, file: str = None):
                 json_data = json.load(f)
                 with open(file=reformat_file, mode='w', encoding='utf-8') as wf:
                     json.dump(json_data, wf, indent=4)
+                    file_list.append(reformat_file)
+
+    return file_list
