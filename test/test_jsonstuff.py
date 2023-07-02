aff54ebfcdb6cfd76928d24b8344d4cd239b50c7
Ethan Hunt <chessking94@gmail.com>
2023-07-02 06:19:03 -0500

    Implement tearDown method

--- test/test_jsonstuff.py
+++ test/test_jsonstuff.py
@@ -11,11 +11,8 @@ class TestJsonstuff(unittest.TestCase):
     def setUp(self):
         self.file_list = []
 
-    def _resetfilelist(self):
-        self.file_list = []
-
-    def _deletefiles(self, files: list):
-        for f in files:
+    def tearDown(self):
+        for f in self.file_list:
             if os.path.isfile(f):
                 os.remove(f)
 
@@ -26,26 +23,21 @@ class TestJsonstuff(unittest.TestCase):
         self.assertRaises(FileNotFoundError, jsonstuff.reformat_json, os.getcwd(), 'dne.json')
 
     def test_reformat_json_single_file(self):
-        self._resetfilelist()
         fname = 'jsonstuff_test1.json'
         self.file_list = jsonstuff.reformat_json(FILE_DIR, fname)
 
         self.assertEqual(len(self.file_list), 1)
-        self._deletefiles(self.file_list)
 
     def test_reformat_json_already_formatted(self):
-        self._resetfilelist()
         fname = 'jsonstuff_test2.json'
         self.file_list = jsonstuff.reformat_json(FILE_DIR, fname)
 
         self.assertEqual(len(self.file_list), 0)
 
     def test_reformat_json_multiple_files(self):
-        self._resetfilelist()
         self.file_list = jsonstuff.reformat_json(FILE_DIR)
 
-        self.assertEqual(len(self.file_list), 2)
-        self._deletefiles(self.file_list)
+        self.assertEqual(len(self.file_list), 3)
 
 
 if __name__ == '__main__':
