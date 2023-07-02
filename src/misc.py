c38d4471a02a9bf3be6851793608518b493e24f8
Ethan Hunt <chessking94@gmail.com>
2023-07-02 06:19:38 -0500

    Implement misc testing

--- src/misc.py
+++ src/misc.py
@@ -11,11 +11,17 @@ import csv
 import json
 import os
 
+from .constants import VALID_DELIMS as VALID_DELIMS
 
-def get_config(module: str, key: str):
-    config_path = os.path.dirname(__file__)
-    for _ in range(1):
-        config_path = os.path.dirname(config_path)
+
+def get_config(module: str, key: str, path_override: str = None) -> str:
+    # TODO: Would it make more sense to have the config path be a parameter somewhere? Global variable? Config parameter?
+    if path_override is None:
+        config_path = os.path.dirname(__file__)
+        for _ in range(1):  # predefined to be one directory above the location of this file
+            config_path = os.path.dirname(config_path)
+    else:
+        config_path = path_override
     # TODO: Generalize this so it can accept both JSON and YAML, possibly even a two column csv/txt file
     with open(os.path.join(config_path, f'{module}_config.json'), 'r') as t:
         key_data = json.load(t)
@@ -23,16 +29,19 @@ def get_config(module: str, key: str):
     return val
 
 
-def csv_to_json(csvfile: str) -> dict:
+def csv_to_json(csvfile: str, delimiter: str = ',') -> dict:
     """
     Return a nested dictionary object from a csv where the first column is
     the key and subsequent columns are nested key:value pairs for that key
     """
+    if delimiter not in VALID_DELIMS:
+        raise NotImplementedError(f"invalid delimiter: {delimiter}")
+
     nested_dict = defaultdict(dict)
     key_set = set()
 
     with open(csvfile, mode='r', encoding='utf-8') as file:
-        reader = csv.reader(file)
+        reader = csv.reader(file, delimiter=delimiter)
         headers = next(reader)  # Read the header row
 
         for row in reader:
---
+++ test/test_misc.py
@@ -0,0 +1,37 @@
+# import logging
+import os
+import unittest
+
+import src.misc as misc
+
+FILE_DIR = os.path.join(os.path.dirname(__file__), 'files')
+
+
+class TestMisc(unittest.TestCase):
+    def test_get_config_no_file(self):
+        self.assertRaises(FileNotFoundError, misc.get_config, 'Test', 'Test')
+
+    def test_get_config_missing_key(self):
+        test_val = misc.get_config('test', 'DNE', FILE_DIR)
+        self.assertIsNone(test_val)
+
+    def test_get_config(self):
+        test_val = misc.get_config('test', 'Key', FILE_DIR)
+        self.assertEqual(test_val, 'Value')
+
+    def test_csv_to_json(self):
+        csvfile = os.path.join(FILE_DIR, 'csvjsonconvert.csv')
+        csv_dict = {'value1': {'column2': 'value2', 'column3': 'value3'}}
+        self.assertEqual(misc.csv_to_json(csvfile), csv_dict)
+
+    def test_csv_to_json_dup_key(self):
+        csvfile = os.path.join(FILE_DIR, 'csvjsonconvert_fail.csv')
+        self.assertRaises(ValueError, misc.csv_to_json, csvfile)
+
+    def test_csv_to_json_invalid_delim(self):
+        csvfile = os.path.join(FILE_DIR, 'csvjsonconvert.csv')
+        self.assertRaises(NotImplementedError, misc.csv_to_json, csvfile, 'a')
+
+
+if __name__ == '__main__':
+    unittest.main()
