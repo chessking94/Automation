0cd7177ea3f527ea613cc034510ab11906df1c60
Ethan Hunt <chessking94@gmail.com>
2023-07-16 15:21:41 -0500

    Docstrings for fileproc

--- src/fileproc.py
+++ src/fileproc.py
@@ -1,14 +1,12 @@
 """fileproc
 
 Author: Ethan Hunt
-Date: 2023-06-18
-Version: 1.0
+Creation Date: 2023-06-18
 
 """
 
 import csv
 import datetime as dt
-import logging
 import os
 import tempfile
 import shutil
@@ -21,12 +19,61 @@ from .misc import get_config as get_config
 # TODO: Handling/logging around receiving servicer files
 
 
-class monitoring_constants:
+class fileproc_constants:
+    """A class for constants necessary for the fileproc module"""
     MODULE_NAME = os.path.splitext(os.path.basename(__file__))[0]
 
 
 class monitoring:
+    """Class to monitor directories for changes in content
+
+    Attributes
+    ----------
+    path : str
+        Directory to monitor
+    config_path : str
+        Location of library configuration file
+    error : str
+        Text of validation error
+    last_review_time : str
+        Reference datetime to identify modified files from. Formatted as yyyy-mm-dd HH:MM:SS
+    manual_review : bool
+        Flag used if process is being run as a one-off instance
+    log_path : str
+        Directory in which log files will write to. Defined in the configuration file and will always be root/module_name
+    log_name : str
+        Name of log file, always module_yyyymmddHHMMSS.log
+    log_delim : str
+        Delimiter to use in the log file, defined in the configuration file
+    ref_delim : str
+        Delimiter to use in the reference file logging the last datetime of monitoring review, defined in the configuration file
+
+    """
     def __init__(self, path: str, config_path: str = None):
+        """Inits monitoring class
+
+        Parameters
+        ----------
+        path : str
+            Directory to monitor
+        config_path : str, optional (default None)
+            Location of library configuration file
+
+        Raises
+        ------
+        FileNotFoundError
+            If 'config_path' does not exist
+            If 'path' does not exist
+        RuntimeError
+            If 'path' contains 'ref_delim'
+
+        Examples
+        ----------
+        >>> monitor = monitoring('/my/path')
+        >>> print(monitor.__module__)
+        src.fileproc
+
+        """
         if config_path and not os.path.isdir(config_path):
             raise FileNotFoundError
 
@@ -36,25 +83,45 @@ class monitoring:
         self.last_review_time = self._processtime(readwrite='r')
         self.manual_review = False
 
-        self.log_path = os.path.join(get_config('logRoot', config_path), monitoring_constants.MODULE_NAME)
+        self.log_path = os.path.join(get_config('logRoot', config_path), fileproc_constants.MODULE_NAME)
         self.log_name = f"{self.__class__.__name__}_{dt.datetime.now().strftime('%Y%m%d%H%M%S')}.log"
 
         self.log_delim = get_config('logDelimiter', config_path)
         self.ref_delim = get_config('fileproc_referenceDelimiter', config_path)
 
-        if self._validate() is not None:
-            logging.critical(self.error)
+        self._validate()
 
-    def _validate(self) -> str:
+    def _validate(self):
+        """Class function to validate if the necessary information is available at the time of __init__"""
         if not os.path.isdir(self.path):
-            self.error = f'Path does not exist|{self.path}'
+            raise FileNotFoundError(f"path '{self.path}' does not exist")
 
         if self.ref_delim in self.path:
-            self.error = f'Path contains referenceDelimiter|{self.path}'
-
-        return self.error
+            raise RuntimeError(f"path '{self.path}' contains referenceDelimiter '{self.ref_delim}'")
 
     def _processtime(self, readwrite: str = 'r', dt_string: str = None) -> dt.datetime:
+        """Class function that returns the time a directory was last monitored
+
+        Read from file the last datetime a directory was monitored, or update file with new datetime
+        Able to accept a custom time to bypass reading from file
+
+        Parameters
+        ----------
+        readwrite : str, optional (default 'r')
+            Indicator whether to Read from reference file or Write to reference file
+        dt_string : str, optional (default None)
+            Datetime override value to use instead of datetime in reference file
+
+        Returns
+        -------
+        Datetime.Datetime : 'dt_string' converted to Datetime object or value from reference file
+
+        Raises
+        ------
+        ValueError
+            If 'readwrite' is not either "r" or "w"
+
+        """
         valid_readwrite = ['r', 'w']
         if readwrite not in valid_readwrite:
             raise ValueError(f"Invalid parameter 'readwrite': {readwrite}")
@@ -67,9 +134,11 @@ class monitoring:
         except (TypeError, ValueError):
             dt_format_validated = False
 
+        # valid datetime was provided; return it and exit function
         if dt_format_validated:
             return time_val
 
+        # create reference file if it does not exist
         key_column = 'Path'
         dt_column = 'LastMonitorTime'
         reference_file = get_config('fileproc_referenceFile', self.config_path)
@@ -78,8 +147,8 @@ class monitoring:
             with open(file=reference_file, mode='w', encoding='utf-8') as f:
                 f.write(header_row)
 
-        time_val = None
         if readwrite == 'r':
+            # Read last time from reference file; return Epoch if no datetime is found
             with open(reference_file, 'r') as ref_file:
                 reader = csv.DictReader(ref_file)
                 for row in reader:
@@ -90,6 +159,7 @@ class monitoring:
             if time_val is None:
                 time_val = dt.datetime(1970, 1, 1)
         else:
+            # Write current datetime to reference file
             temp_file_path = tempfile.NamedTemporaryFile(delete=False).name
             with open(reference_file, 'r', newline='') as reader_file, open(temp_file_path, 'w', newline='') as writer_file:
                 reader = csv.DictReader(reader_file)
@@ -115,16 +185,30 @@ class monitoring:
         return time_val
 
     def _writelog(self, filename: str):
+        """Class function to write to a log file"""
         with open(os.path.join(self.log_path, self.log_name), 'a') as logfile:
             dte, tme = dt.datetime.now().strftime('%Y-%m-%d'), dt.datetime.now().strftime('%H:%M:%S')
             logfile.write(f'{self.path}{self.log_delim}{dte}{self.log_delim}')
             logfile.write(f'{tme}{self.log_delim}{filename}{NL}')
 
     def change_time(self, dt_string: str):
+        """Manually set 'last_review_time' and flip the 'manual_review' flag"""
         self.last_review_time = self._processtime(readwrite='r', dt_string=dt_string)
         self.manual_review = True
 
     def modified_files(self, write_log: bool = False) -> list:
+        """Identify which files have been modified since a preset datetime value
+
+        Parameters
+        ----------
+        write_log : bool, optional (default False)
+            Indicator whether to write the list of modified files to a log file
+
+        Returns
+        -------
+        list : All files recently modified, or an empty list if no files have been modified
+
+        """
         write_log = write_log if write_log in BOOLEANS else False
         directory_files = os.listdir(self.path)
         mod_files = []
