59abdad46766bdd24b0bb5e75d54e0f3a3192641
Ethan Hunt <chessking94@gmail.com>
2023-07-16 15:50:37 -0500

    Add docstrings for secrets

--- src/secrets.py
+++ src/secrets.py
@@ -1,18 +1,57 @@
 """secrets
 
 Author: Ethan Hunt
-Date: 2023-07-04
-Version: 1.0
+Creation Date: 2023-07-04
 
 """
 
 from pykeepass import PyKeePass, pykeepass as pk
 
-# TODO: Consider adding write functionality to Keepass DB, intentionally only implementing read at first
-
 
 class keepass():
+    """Class to interact with a Keepass file where secrets are saved
+
+    Attributes
+    ----------
+    kp : pykeepass.PyKeePass
+        Object representing a Keepass file
+    group_title : str
+        Name of the Keepass group in which the secret(s) are saved
+    group : pykeepass.Group
+        Object representing the data associated with the Keepass group
+    entry : pykeepass.Entry
+        Object representing the data associated with the Keepass entry
+
+    TODO
+    ----
+    Consider adding write functionality to Keepass DB, intentionally only implementing read at first
+    Implement keyfile opening of Keepass file
+
+    """
     def __init__(self, filename: str, password: str, group_title: str, entry_title: str):
+        """Inits keepass class
+
+        Parameters
+        ----------
+        filename : str
+            Full name of Keepass file to use
+        password : str
+            Password to open the Keepass file
+        group_title : str
+            Name of the group to find the entry in
+        entry_title : str
+            Name of the entry to extract information from
+
+        Raises
+        ------
+        ValueError
+            If group is not found or multiple groups are found
+            If entry is not found or multiple entries are found
+        TypeError
+            If the unique group found is not the expected object type
+            If the unique entry found is not the expected object type
+
+        """
         self.kp = PyKeePass(filename=filename, password=password)
         self.group_title = group_title
 
@@ -44,15 +83,56 @@ class keepass():
         return entries[0]
 
     def getgeneral(self, field: str) -> str:
+        """Obtains general data value
+
+        Parameters
+        ----------
+        field : str
+            The name of the field to retrieve information from
+
+        Returns
+        -------
+        str : the value requested
+
+        Raises
+        ------
+        NotImplementedError
+            If 'field' is not an expected value
+
+        TODO
+        ----
+        Investigate if there is a more flexible way to perform validation on 'field'
+
+        """
         field = field.lower()
-        field_list = ['title', 'username', 'password', 'url']  # TODO: Look into a more flexible way to do this validation
+        field_list = ['username', 'password', 'url']
         if field not in field_list:
             raise NotImplementedError(f"field '{field}' is not a valid general field, expecting one of {field_list}")
 
         return getattr(self.entry, field)
 
     def getcustomproperties(self, string_field: str) -> str:
-        # TODO: Look into a more elegant way to do this validation
+        """Obtains custom property value
+
+        Parameters
+        ----------
+        string_field : str
+            The name of the custom field to retrieve information from
+
+        Returns
+        -------
+        str : the value requested
+
+        Raises
+        ------
+        NotImplementedError
+            If string_field is not an expected value for that Keepass group
+
+        TODO
+        ----
+        Investigate if there is a more flexible way to perform validation on 'property_list'
+
+        """
         property_list = []
         if self.group_title == 'test':
             property_list = ['Test']
@@ -85,6 +165,23 @@ class keepass():
         return props.get(string_field)
 
     def readattachment(self, attachment_name: str) -> str:
+        """Obtains text of an attachment
+
+        Parameters
+        ----------
+        attachment_name : str
+            The name of the attachment to read
+
+        Returns
+        -------
+        str : the attachment text itself
+
+        Raises
+        ------
+        IndexError
+            If 'attachment_name' does not exist in the Keepass entry
+
+        """
         attach = self.entry.attachments
         data = None
         for a in attach:
